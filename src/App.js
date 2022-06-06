6ed1bf5c7bccffcc128309c4a71e79a2a3622f01
Pablo Marino <pmarino2013@gmail.com>
2022-06-06 12:20:41 -0300

    Inicializando template

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import "./App.css";
-import IntroApp from "./components/IntroApp";
 
 function App() {
-  const usuario = {
-    id: 1,
-    username: "alvaro2022",
-    pass: "123456win",
-  };
-
-  const roles = ["Admin", "User", "Developer"];
-
   return (
     <div className="App">
       <h1>Intro App</h1>
       <hr />
-      <IntroApp dato={usuario} roles={roles} />
     </div>
   );
 }
